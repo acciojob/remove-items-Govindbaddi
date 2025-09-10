f08c23bd34a7356e3a6fd115f9bf09c1058e4f40
Govindbaddi <baddigovindarao710@gmail.com>
2025-09-10 09:44:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ select.addEventListener("click",removecolor)
 function removecolor() {
 	const selectedIndex=selectColor.selectedIndex
 	//selectedoption.remove()
-	if(selectedIndex>0){
+	
 	selectColor.remove(selectedIndex)
-	}
+	
 	
 }
 
